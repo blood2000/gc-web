4765d56ed255ef63c759346d884bb3f200fda5c2
huangst <313949553@qq.com>
2021-09-13 16:35:02 +0800

    feat：地图布局框架

---
+++ src/views/map/components/map.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- 地图组件 -->
+  <div class="c-map-box">
+    <div id="device-map-container" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      map: null,
+    }
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    /** 初始化地图 */
+    initMap() {
+      this.map = new AMap.Map('device-map-container', {
+        resizeEnable: true,
+        center: [119.358267, 26.04577],
+        zoom: 11
+      });
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.c-map-box{
+  height: 100%;
+  position: relative;
+  >#device-map-container{
+    height: 100%;
+  }
+}
+</style>
---
+++ src/views/map/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- 设备管理-电子围栏 -->
+  <div class="map-container ly-flex">
+    <div class="map-container__left">
+        list
+    </div>
+    <div class="map-container__right">
+      <!-- 地图 -->
+      <MapBox ref="mapRef" class="map-box" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MapBox from './components/map.vue';
+export default {
+  name: 'MapInfo',
+  components: {
+    MapBox
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.map-container{
+  margin: 0 15px 0;
+  box-shadow: 0px 2px 3px 0px rgba(51, 153, 255, 0.1);
+  height: calc(100vh - 146px);
+  min-width: 1320px;
+  overflow-x: auto;
+  
+  &__left{
+    width: 364px;
+    height: 100%;
+    padding: 0;
+    background-color: #F8F8F8;
+    border-radius: 4px;
+    overflow: hidden;
+  }
+
+  &__right{
+    width: calc(100% - 364px);
+    height: 100%;
+    padding: 16px 12px 12px;
+    background: #fff;
+    border-radius: 3px;
+  }
+}
+</style>
