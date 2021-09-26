c7b9a3849a97a6645450b35d079f08c6c6b69f9e
chenkc <501617031@qq.com>
2021-09-26 18:03:53 +0800

    1

--- src/views/map/index.vue
+++ src/views/map/index.vue
@@ -287,10 +287,7 @@
     />
 
     <!-- 派车 -->
-    <DispatchVehicle
-      v-if="showDispatchVehicle"
-      class="dispatch-vehicle-panel"
-    />
+    <DispatchVehicle/>
     <!-- 车辆详情 -->
 
     <!-- 轨迹回放 -->
@@ -1485,20 +1482,6 @@ export default {
     height: calc(100% - #{$header-height} - #{$bottom} - 12px);
   }
 
-  //车辆详情
-  > .dispatch-vehicle-panel {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 780px;
-    height: 636px;
-    background: #ffffff;
-    border: 1px solid #e4ecf4;
-    box-shadow: 0px 12px 48px rgba(0, 0, 0, 0.03);
-    border-radius: 4px;
-    z-index: 1000;
-  }
 
   // 行车轨迹
   > .track-list-panel {
