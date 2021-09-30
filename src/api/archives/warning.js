7f0e9c4d1fd6a545420d304b82131f03a8d2e1d7
wuyp <wuyuanping2000@outlook.com>
2021-09-30 11:26:21 +0800

    09-30 告警列表设备类型接口

--- src/api/archives/warning.js
+++ src/api/archives/warning.js
@@ -1,6 +1,7 @@
 export default {
   warning_list:'/fmsweb/alarm/v1/webAlarmNotificationList', //查询告警信息列表
   warningType_list: '/fmsweb/alarmTypeInfo/v1/alarmTypeInfoList',   //告警类型列表
+  deviceType_list: '/fmsweb/alarm/deviceSeriesModelInfo/v1/deviceSeriesModelInfoList',   //设备类型列表
   warning_detail:(code)=>{
     return `/fmsweb/alarm/v1/getAlarmNotificationDetailsById/${code}`
   }, //车辆详情
