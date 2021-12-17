bf97c27852642e9e634fddfc4ed849e8d663e3a4
陈忠心 <zx.chen@dface.cn>
2021-12-17 11:56:50 +0800

    feat: 常用路线开发完成

---
+++ src/views/archives/route/route_config.js
@@ -0,0 +1,31 @@
+export default {
+  routeName: '', // 路线名称
+  loadingAddressType: '1', //1 装货  2 卸货
+  loadingAddressProvince: '', // 出发地省名称
+  loadingAddressProvinceCode: '', // 出发地省编码
+  loadingAddressCity: '', // 出发地市名称
+  loadingAddressCityCode: '', // 出发地市编码
+  loadingAddressDistrict: '', // 出发地区名称
+  loadingAddressDistrictCode: '', // 出发地区编码
+  loadingAddressDetail: '', // 出发地详细地址
+  loadingAddressAlias: '', // 出发地址别名
+  loadingAddressLng: '', // 出发地经度
+  loadingAddressLat: '', // 出发地纬度
+  loadingAddressContact: '', // 出发地联系人
+  loadingAddressContactPhone: '', // 出发地联系电话
+  loadingWhetherToAdd: 0, // 出发地是否添加到常用地址：0否1是
+  unloadingAddressType: '2', //1 装货  2 卸货
+  unloadingAddressProvince: '', // 卸货地省名称
+  unloadingAddressProvinceCode: '', // 卸货地省编码
+  unloadingAddressCity: '', // 卸货地市名称
+  unloadingAddressCityCode: '', // 卸货地市编码
+  unloadingAddressDistrict: '', // 卸货地区名称
+  unloadingAddressDistrictCode: '', // 卸货地区编码
+  unloadingAddressDetail: '', // 出发地详细地址
+  unloadingAddressAlias: '', // 卸货地址别名
+  unloadingAddressLng: '', // 卸货地经度
+  unloadingAddressLat: '', // 卸货地纬度
+  unloadingAddressContact: '', // 卸货地地联系人
+  unloadingAddressContactPhone: '', // 卸货地联系电话
+  unloadingWhetherToAdd: 0 // 卸货地是否添加到常用地址：0否1是
+}
