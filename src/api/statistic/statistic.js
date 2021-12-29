34c4ec52f256d882589a37ba808cc44a4ac05bbf
chenkc <501617031@qq.com>
2021-12-29 18:38:45 +0800

    123

--- src/api/statistic/statistic.js
+++ src/api/statistic/statistic.js
@@ -18,5 +18,17 @@ export default {
   pagingWeighbridgeStatisticsByCode:
     '/fmsweb/basic/weighbridge/v1/pagingWeighbridgeStatisticsByCode', // 磅单统计二级
   pagingLastWeighbridgeStatistics:
-    '/fmsweb/basic/weighbridge/v1/pagingLastWeighbridgeStatistics' // 磅单统计三级
+    '/fmsweb/basic/weighbridge/v1/pagingLastWeighbridgeStatistics', // 磅单统计三级
+    userOverView: '/fmsweb/basic/largeScreen/v1/userOverView', // 用户总览
+    getCityTotal: '/fmsweb/basic/largeScreen/v1/getCityTotal', // 地区分布
+    vehicleType: '/fmsweb/basic/largeScreen/v1/vehicleType', // 车辆类型
+    totalScreen: '/fmsweb/basic/largeScreen/v1/totalScreen', // 大屏中心数据统计
+    deviceScreenTotal: '/fmsweb/basic/largeScreen/v1/deviceScreenTotal', // 设备资产信息
+    deviceTypeAccounted: '/fmsweb/basic/largeScreen/v1/DeviceTypeAccounted', // 设备占比
+    organizationAlarm: '/fmsweb/dashboard/tempAlarm/v1/organizationAlarm', // 组织告警排行
+    illegalVehicle: '/fmsweb/dashboard/tempAlarm/v1/illegalVehicle', // 违规车辆排行
+    illegalDriver: '/fmsweb/dashboard/tempAlarm/v1/illegalDriver', // 违规司机排行
+    alarmTypeStatistics: '/fmsweb/dashboard/tempAlarm/v1/alarmTypeStatistics', // 告警类型统计
+    alarmInfo: '/fmsweb/dashboard/tempAlarm/v1/alarmInfo', // 获取告警信息
+    thermodynamicDiagramAlarm: '/fmsweb/dashboard/tempAlarm/v1/thermodynamicDiagramAlarm' // 热力图
 }
