82e463c7e8def8f2f43527090921cbeaf7007895
陈忠心 <zx.chen@dface.cn>
2021-12-30 17:05:31 +0800

    feat: 车务管理模块完成

--- src/api/archives/setting.js
+++ src/api/archives/setting.js
@@ -1,6 +1,7 @@
 export default {
   userAlarmTypeInfoList: '/fmsweb/alarmTypeInfo/v1/userAlarmTypeInfoList', // 所有的告警类型
-  selectUserAlarmTypeInfoList:
-    '/fmsweb/alarmTypeInfo/v1/selectUserAlarmTypeInfoList', // 选中的告警类型
-  addAlarmTypeUser: '/fmsweb/alarmTypeUser/v1/addAlarmTypeUser' // 新增告警类型
+  selectUserAlarmTypeInfoList: '/fmsweb/alarmTypeInfo/v1/selectUserAlarmTypeInfoList', // 选中的告警类型
+  addAlarmTypeUser: '/fmsweb/alarmTypeUser/v1/addAlarmTypeUser', // 新增告警类型
+  getCommandType: '/fmsweb/alarm/v1/getCommandType', // 查询告警阈值
+  sendCommand: '/fmsweb/alarm/v1/sendCommand', // 保存告警阈值
 }
